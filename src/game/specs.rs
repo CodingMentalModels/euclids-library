6b5b064fbbc87decfcb11b5bc8b6a45a6498330d
codingmentalmodels <codingmentalmodels@gmail.com>
2023-09-07 15:13:33 +0200

    Set up generic SpecLookups

--- src/game/specs.rs
+++ src/game/specs.rs
@@ -1,4 +1,17 @@
-use std::collections::HashSet;
+use std::collections::{BTreeMap, HashSet};
 
-use bevy::prelude::Entity;
+use bevy::prelude::*;
 use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Resource)]
+pub struct SpecLookup<T: PartialEq + Eq>(BTreeMap<String, T>);
+
+impl<T: PartialEq + Eq> SpecLookup<T> {
+    pub fn get(&self, name: &str) -> Option<&T> {
+        self.0.get(name)
+    }
+
+    pub fn get_names(&self) -> HashSet<&String> {
+        self.0.keys().collect()
+    }
+}
