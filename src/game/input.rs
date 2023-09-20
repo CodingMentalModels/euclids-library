645ba2257993b16ea8bf67eaeb88e884c2fe50d8
codingmentalmodels <codingmentalmodels@gmail.com>
2023-09-20 21:22:40 +0200

    Implement move camera system but it doesnt work because the text camera is via the UI

--- src/game/input.rs
+++ src/game/input.rs
@@ -3,7 +3,7 @@ use bevy_mod_raycast::{
     print_intersections, DefaultRaycastingPlugin, RaycastMethod, RaycastSource, RaycastSystem,
 };
 
-use super::events::{Direction, MovementEvent};
+use super::events::{CameraMovementEvent, Direction, MovementEvent};
 use super::resources::GameState;
 
 pub struct InputPlugin;
@@ -12,6 +12,7 @@ impl Plugin for InputPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<PauseUnpauseEvent>()
             .add_event::<MovementEvent>()
+            .add_event::<CameraMovementEvent>()
             .add_systems(
                 First,
                 update_raycast_with_cursor.before(RaycastSystem::BuildRays::<MouseoverRaycastSet>),
@@ -38,6 +39,7 @@ pub fn input_system(
     state: Res<State<GameState>>,
     mut pause_unpause_event_writer: EventWriter<PauseUnpauseEvent>,
     mut movement_event_writer: EventWriter<MovementEvent>,
+    mut camera_movement_event_writer: EventWriter<CameraMovementEvent>,
 ) {
     if keyboard_input.just_pressed(KeyCode::Escape) {
         pause_unpause_event_writer.send(PauseUnpauseEvent);
@@ -45,7 +47,8 @@ pub fn input_system(
 
     match state.get() {
         GameState::Exploring => {
-            handle_movement(keyboard_input, movement_event_writer);
+            handle_movement(&keyboard_input, movement_event_writer);
+            handle_camera_movement(&keyboard_input, camera_movement_event_writer);
         }
         _ => {}
     }
@@ -75,7 +78,7 @@ pub struct MouseoverRaycastSet;
 // Helper Functions
 
 fn handle_movement(
-    keyboard_input: Res<Input<KeyCode>>,
+    keyboard_input: &Res<Input<KeyCode>>,
     mut movement_event_writer: EventWriter<MovementEvent>,
 ) {
     if keyboard_input.just_pressed(KeyCode::Numpad8) {
@@ -108,6 +111,24 @@ fn handle_movement(
 
     if keyboard_input.just_pressed(KeyCode::Numpad3) {
         movement_event_writer.send(MovementEvent(Direction::DownRight));
+    }
+}
+
+fn handle_camera_movement(
+    keyboard_input: &Res<Input<KeyCode>>,
+    mut camera_movement_event_writer: EventWriter<CameraMovementEvent>,
+) {
+    if keyboard_input.just_pressed(KeyCode::W) {
+        camera_movement_event_writer.send(CameraMovementEvent(Direction::Up));
+    }
+    if keyboard_input.just_pressed(KeyCode::A) {
+        camera_movement_event_writer.send(CameraMovementEvent(Direction::Left));
+    }
+    if keyboard_input.just_pressed(KeyCode::S) {
+        camera_movement_event_writer.send(CameraMovementEvent(Direction::Down));
+    }
+    if keyboard_input.just_pressed(KeyCode::D) {
+        camera_movement_event_writer.send(CameraMovementEvent(Direction::Right));
     }
 }
 
