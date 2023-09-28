c21d552649d1a0b08974edd76ed993dce5841c94
codingmentalmodels <codingmentalmodels@gmail.com>
2023-09-28 17:58:22 +0200

    Stub out BodyPartTree and its constituants

--- src/game/player.rs
+++ src/game/player.rs
@@ -41,4 +41,117 @@ impl Player {
     }
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BodyPartTreeNode {
+    body_part: BodyPart,
+    children: Box<Vec<BodyPartTreeNode>>,
+}
+
+impl BodyPartTreeNode {
+    pub fn new(body_part: BodyPart, children: Vec<BodyPartTreeNode>) -> Self {
+        Self {
+            body_part,
+            children: Box::new(children),
+        }
+    }
+
+    pub fn leaf(body_part: BodyPart) -> Self {
+        Self::new(body_part, Vec::new())
+    }
+
+    pub fn new_humanoid() -> Self {
+        let mut body = Self::leaf(BodyPartType::Body.into());
+        body.add(Self::new_humanoid_head());
+
+        body.add(Self::new_leg_and_foot());
+        body.add(Self::new_leg_and_foot());
+
+        body.add(Self::new_arm_and_hand());
+        body.add(Self::new_arm_and_hand());
+
+        body
+    }
+
+    pub fn new_humanoid_head() -> Self {
+        let mut head = Self::leaf(BodyPartType::Head.into());
+
+        head.add_leaf(BodyPartType::Eye.into());
+        head.add_leaf(BodyPartType::Eye.into());
+
+        head.add_leaf(BodyPartType::Ear.into());
+        head.add_leaf(BodyPartType::Ear.into());
+
+        head.add_leaf(BodyPartType::Mouth.into());
+
+        head
+    }
+
+    pub fn new_leg_and_foot() -> Self {
+        let mut leg = Self::leaf(BodyPartType::Leg.into());
+        leg.add_leaf(BodyPartType::Foot.into());
+        leg
+    }
+
+    pub fn new_arm_and_hand() -> Self {
+        let mut arm = Self::leaf(BodyPartType::Arm.into());
+        arm.add_leaf(BodyPartType::Hand.into());
+        arm
+    }
+
+    pub fn add(&mut self, body_part: BodyPartTreeNode) {
+        self.children.push(body_part);
+    }
+
+    pub fn add_leaf(&mut self, body_part: BodyPart) {
+        self.add(Self::leaf(body_part))
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BodyPart {
+    body_part_type: BodyPartType,
+    statuses: Vec<BodyPartStatus>,
+}
+
+impl From<BodyPartType> for BodyPart {
+    fn from(value: BodyPartType) -> Self {
+        Self::new_empty(value)
+    }
+}
+
+impl BodyPart {
+    pub fn new(body_part_type: BodyPartType, statuses: Vec<BodyPartStatus>) -> Self {
+        Self {
+            body_part_type,
+            statuses,
+        }
+    }
+
+    pub fn new_empty(body_part_type: BodyPartType) -> Self {
+        Self::new(body_part_type, Vec::new())
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum BodyPartType {
+    Body,
+    Head,
+    Brain,
+    Eye,
+    Ear,
+    Mouth,
+    Arm,
+    Hand,
+    Leg,
+    Foot,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum BodyPartStatus {
+    Bleeding(u32),
+    Infected,
+    Cancerous,
+    Nonfunctional,
+}
+
 // End Structs
