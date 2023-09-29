98941899f13b5df5a77eb82f3df7d7664cac3e1f
codingmentalmodels <codingmentalmodels@gmail.com>
2023-09-29 15:55:24 +0200

    Implement story

--- src/game/dialog.rs
+++ src/game/dialog.rs
@@ -17,6 +17,10 @@ impl Dialog {
     pub fn new_npc(speaker: String, contents: String, next: Option<Dialog>) -> Self {
         Self::NPCDialog(Box::new(NPCDialog::new(speaker, contents, next)))
     }
+
+    pub fn story(speaker: String, contents: Vec<String>) -> Self {
+        NPCDialog::story(speaker, contents).into()
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
@@ -37,5 +41,23 @@ impl NPCDialog {
 
     pub fn leaf(speaker: String, contents: String) -> Self {
         Self::new(speaker, contents, None)
+    }
+
+    pub fn story(speaker: String, contents: Vec<String>) -> Self {
+        if contents.len() == 0 {
+            panic!("A story must have at least one element.");
+        }
+
+        if contents.len() == 1 {
+            Self::leaf(speaker, contents[0].clone())
+        } else {
+            let current_contents = contents[0].clone();
+            let additional_contents = contents[1..].to_vec();
+            Self::new(
+                speaker.clone(),
+                current_contents,
+                Some(Self::story(speaker, additional_contents).into()),
+            )
+        }
     }
 }
